a422f75159d47009ceebc542591912ba526a18b5
Álvaro Biguelini <alvaro.jaques.biguilini@gmail.com>
2023-07-05 16:07:30 -0300

    criada a primeira classe: posicao

--- src/aplicacao/programa.java
+++ src/aplicacao/programa.java
@@ -1,9 +1,12 @@
 package aplicacao;
 
+import jogodetabuleiro.posicao;
+
 public class programa {
 
 	public static void main(String[] args) {
-		System.out.println("testelooo");
+		posicao pos = new posicao(3, 2);
+		System.out.println(pos);
 
 	}
 
