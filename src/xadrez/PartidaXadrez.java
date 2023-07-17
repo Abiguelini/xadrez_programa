285c718430c77580ecca431769693ab7fd3af8cf
Álvaro Biguelini <alvaro.jaques.biguilini@gmail.com>
2023-07-17 17:24:54 -0300

    melhorias print

--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -2,6 +2,7 @@ package xadrez;
 
 import jogodetabuleiro.Posicao;
 import jogodetabuleiro.Tabuleiro;
+import xadrez.pecas.Rei;
 import xadrez.pecas.Torre;
 
 public class PartidaXadrez {
@@ -25,8 +26,19 @@ public class PartidaXadrez {
 	}
 	
 	public void SetUpInicial() {
-		Novaposicao('a', 2, new Torre(tabuleiro,Cores.BLACK));
-		Novaposicao('h', 5, new Torre(tabuleiro,Cores.BLACK));
+		Novaposicao('c', 1, new Torre(tabuleiro, Cores.WHITE));
+		Novaposicao('c', 2, new Torre(tabuleiro, Cores.WHITE));
+		Novaposicao('d', 2, new Torre(tabuleiro, Cores.WHITE));
+		Novaposicao('e', 2, new Torre(tabuleiro, Cores.WHITE));
+		Novaposicao('e', 1, new Torre(tabuleiro, Cores.WHITE));
+		Novaposicao('d', 1, new Rei(tabuleiro, Cores.WHITE));
+
+		Novaposicao('c', 7, new Torre(tabuleiro, Cores.BLACK));
+		Novaposicao('c', 8, new Torre(tabuleiro, Cores.BLACK));
+		Novaposicao('d', 7, new Torre(tabuleiro, Cores.BLACK));
+		Novaposicao('e', 7, new Torre(tabuleiro, Cores.BLACK));
+		Novaposicao('e', 8, new Torre(tabuleiro, Cores.BLACK));
+		Novaposicao('d', 8, new Rei(tabuleiro, Cores.BLACK));
 	}
 
 }
